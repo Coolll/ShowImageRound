d0b631e8b1d6a68683fdcf07cc2b9295852af0ed
龙培 <longpei@longpeidemacbook-pro.local>
2016-08-30 08:30:20 +0800

    Initial Commit

---
+++ TestShowImageRound/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestShowImageRound
+//
+//  Created by 龙培 on 16/8/30.
+//  Copyright © 2016年 龙培. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestShowImageRound/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestShowImageRound
+//
+//  Created by 龙培 on 16/8/30.
+//  Copyright © 2016年 龙培. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
