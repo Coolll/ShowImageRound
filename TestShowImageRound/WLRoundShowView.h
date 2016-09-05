1c8ef5793f0059680ba66a5b9453057de2c1edaa
龙培 <longpei@longpeideMacBook-Pro.local>
2016-09-05 09:57:38 +0800

    first commit

---
+++ TestShowImageRound/WLRoundShowView.h
@@ -0,0 +1,13 @@
+//
+//  WLRoundShowView.h
+//  TestShowImageRound
+//
+//  Created by 龙培 on 16/8/30.
+//  Copyright © 2016年 龙培. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLRoundShowView : UIView
+
+@end
---
+++ TestShowImageRound/WLRoundShowView.m
@@ -0,0 +1,21 @@
+//
+//  WLRoundShowView.m
+//  TestShowImageRound
+//
+//  Created by 龙培 on 16/8/30.
+//  Copyright © 2016年 龙培. All rights reserved.
+//
+
+#import "WLRoundShowView.h"
+
+@implementation WLRoundShowView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
